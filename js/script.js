a5c0385f83ad6867053606dfcdc9b3e144d407ae
Mintu Moni Kurmi <39824383+mintukurmi@users.noreply.github.com>
2019-02-24 20:30:11 +0530

    Login-signup page added & OTP page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function ShowLoginForm(){
+		document.getElementById("signup").style.display = "none";
+		document.getElementById("login").style.display = "block";
+			
+	}
+
+function ShowSignupForm(){
+		document.getElementById("login").style.display = "none";
+		document.getElementById("signup").style.display = "block";
+			
+	}
