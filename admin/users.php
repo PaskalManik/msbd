d08ab512ae2a81d654da301fbc23afab78e18fbb
Mintu Moni Kurmi <mintukurmi1@gmail.com>
2019-06-25 20:22:26 +0530

    Users modal added

--- admin/users.php
+++ admin/users.php
@@ -43,8 +43,8 @@
                 <h3 class="text-orange text-center py-1">All Users</h3>
 
 
-                <div class="order px-3 my-4 custom-card">
-                  <div class="row my-5 mx-5 ">
+                <div class="order px-3 my-3 custom-card">
+                  <div class="row my-2 mx-5 ">
                         <table class="table">
     <thead class="thead-dark">
         <tr>
@@ -67,10 +67,33 @@
                               <td>#<?php echo $row['user_id'];  ?></td>
                               <td><?php echo $row['firstname'].' '.$row['lastname']  ?></td> 
                               <td> 
-                              <button class="btn custom-btn my-4"> <i class="fa fa-eye"></i> View</button>
+                              <button class="btn custom-btn my-4" data-toggle="modal" data-target="#userInfoModal<?php echo $row['user_id'];  ?>"> <i class="fa fa-eye"></i> View</button>
                               </div></td> 
                          </tr>
-
+						<!-- Modal -->
+						<div class="modal fade" id="userInfoModal<?php echo $row['user_id'];  ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						  <div class="modal-dialog" role="document">
+						    <div class="modal-content">
+						      <div class="modal-header">
+						        <h5 class="modal-title" id="exampleModalLabel"><?php echo $row['firstname']?>'s Info</h5>
+						        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						          <span aria-hidden="true">&times;</span>
+						        </button>
+						      </div>
+						      <div class="modal-body">
+						        <p>User id: <?php echo $row['user_id'];  ?></p>
+						        <p>Firstname: <?php echo $row['firstname']?></p>
+						        <p>Lastname: <?php echo $row['lastname']?></p>
+						        <p>Email: <?php echo $row['email']?></p>
+						        <p>Contact No.: <?php echo $row['contact_no']?></p>
+						      </div>
+						      <div class="modal-footer">
+						        <button type="button" class="btn btn-secondary" data-dismiss="modal"> <i class="fa fa-close"></i> Close</button>
+						        <button type="button" class="btn custom-btn"> <i class="fa fa-envelope-o"></i> Email</button>
+						      </div>
+						    </div>
+						  </div>
+						</div>
 
                       <?php
 
